3d452d5f9fb59498b7de5d7ecd029f759c3ee797
beemaster1972 <ugor.finno@yandex.ru>
2023-12-22 13:16:15 +0500

    seminar 2 getStringLengthN

---
+++ Seminar2/src/getStringLengthN.java
@@ -0,0 +1,17 @@
+
+public class getStringLengthN {
+    public static void main(String[] args) {
+        char char1 = 'a', char2 = 'b';
+        int N = 100;
+        String string = getStringLenN(N, char1, char2);
+        System.out.printf("%s имеет длину %d",string, string.length());
+
+    }
+    public static String getStringLenN(int N, Character c1, Character c2){
+        if ((N % 2) == 1) return "Не чётная длина строки!";
+        StringBuilder result = new StringBuilder(N);
+
+        for (int i=0;i<N/2;i++) result.append(c1.toString() + c2.toString());
+        return result.toString();
+    }
+}
